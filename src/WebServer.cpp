ba858ead636998304b134387a09134f332f459bf
dmmosh <evilsharkeater@gmail.com>
2025-12-10 22:19:58 -0500

    nothing to note

--- src/WebServer.cpp
+++ src/WebServer.cpp
@@ -18,8 +18,7 @@ void WebServer::onMessageReceived(int client, const char* msg, int length){
 
     */
    std::ifstream f("../www/index.html", std::ios::binary | std::ios::ate); // reead from file
-   unsigned long l = f.tellg();
-   std::cout << l << '\n';
+   
    std::ostringstream oss; // output stream
    oss  <<                 "HTTP/1.1 200 OK\r\n"
                            "Content-Type: text/html; charset=UTF-8\r\n"
@@ -29,7 +28,7 @@ void WebServer::onMessageReceived(int client, const char* msg, int length){
 
     
 
-   sendToClient(client, oss.str().c_str(), 88+l);
+   sendToClient(client, oss.str().c_str(), oss.str().length());
    f.close();
 }; 
 
