c98999a6d745799dbfbd0f7bc75b8150b90acf3f
dmmosh <evilsharkeater@gmail.com>
2025-12-10 21:13:30 -0500

    nothing to note

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,11 @@
 int main()
 {
     std::cout<< "running\n";
-    WebServer webServer("127.0.0.1", 8080);
+    WebServer webServer("0.0.0.0", 8080);
     if(webServer.init() != 0){
         return 0;
     }
     webServer.run();
     system("pause");
-    return 0;
+    
 }
