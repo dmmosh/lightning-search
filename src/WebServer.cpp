08f1f8293ab47cff1a338b37586c5c5a3320fa8d
dmmosh <evilsharkeater@gmail.com>
2025-12-11 00:20:40 -0500

    nothing to note

--- src/WebServer.cpp
+++ src/WebServer.cpp
@@ -30,7 +30,7 @@ void WebServer::onMessageReceived(int client, const char* msg, int length){
    std::ifstream f; // reead from file
    unsigned long size = 9;
 
-   if(parsed.size() >= 2 && parsed[0] == "GET"){ // < request type > < file or endpoint > < http type >
+   if(parsed.size() >= 3 && parsed[0] == "GET"){ // < request type > < file or endpoint > < http type >
     std::cout << parsed[1] << "\n";
 
         if(parsed[1] == "/"){ // reroute to home page 
