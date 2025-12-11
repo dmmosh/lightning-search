4c879a208478b3b72d2611d8fe58362126e883c9
dmmosh <evilsharkeater@gmail.com>
2025-12-10 21:29:02 -0500

    nothing to note

--- src/WebServer.cpp
+++ src/WebServer.cpp
@@ -19,9 +19,9 @@ void WebServer::onMessageReceived(int client, const char* msg, int length){
    std::string out = "HTTP/1.1 200 OK\r\n"
                             "Cache-Control: no-cache, private\r\n"
                            "Content-Type: text/html; charset=UTF-8\r\n"
-                           "Content-Length: 12\r\n"
+                           "Content-Length: 14\r\n"
                            "\r\n"
-                           "Hello World!";
+                           "<h1>Hello</h1>";
    sendToClient(client, out.c_str(), out.size()+1);
 }; 
 
