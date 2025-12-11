c111e22177e66c6c814ad03a71b0245108d7c723
dmmosh <evilsharkeater@gmail.com>
2025-12-10 21:57:15 -0500

    nothing to note

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ git add .
 git commit -am "nothing to note"
 git push
 
-g++ -std=c++20 src/main.cpp src/WebServer.cpp src/TcpListener.cpp -o ls
+g++ src/main.cpp src/WebServer.cpp src/TcpListener.cpp -o ls
