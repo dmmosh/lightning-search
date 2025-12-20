3c17d80d98636a48457df328d2034d20ba27554f
dmmosh <evilsharkeater@gmail.com>
2025-12-19 22:29:57 -0500

    nothing to note

--- src/WebServer.cpp
+++ src/WebServer.cpp
@@ -90,7 +90,7 @@ void WebServer::onMessageReceived(int client, const char* msg, int length){
 
                                 cpr::Header{{"Content-Type","application/json"},
                                             {"x-api-key", key.c_str()}},
-                                cpr::Body{"{'query': 'hello world', 'type': 'fast'}"}
+                                cpr::Body{"{\"query\": \"hello world\", \"type\": \"fast\"}"}
             );
             cpr::Response r = resp_var.get();
             std::cout << r.text << '\n';
