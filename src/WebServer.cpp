23c34c8b39a5700f22e32728af228ee49ab36974
dmmosh <evilsharkeater@gmail.com>
2025-12-20 00:26:47 -0500

    nothing to note

--- src/WebServer.cpp
+++ src/WebServer.cpp
@@ -131,7 +131,7 @@ void WebServer::onMessageReceived(int client, const char* msg, int length){
    if(!parsed.empty() && parsed[0] == '/'){ // < request type > < file or endpoint > < http type >
 
         if(url[0] == '?'){ // search query , has to be preceded by / because files cant be named as such 
-            resp = sendQuery(url+1); // everything after question mark is searched
+            //resp = sendQuery(url+1); // everything after question mark is searched
             h_num = H_PAGE;
             parsed = "/search.html"; //
         }else if(url[0] == '\0'){ // if nothing , main page
