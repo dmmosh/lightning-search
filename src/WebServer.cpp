0f50f1c85cb23f1cfc9c74ca14d7a221ebedf8fd
dmmosh <evilsharkeater@gmail.com>
2025-12-19 21:07:26 -0500

    nothing to note

--- src/WebServer.cpp
+++ src/WebServer.cpp
@@ -95,7 +95,6 @@ void WebServer::onMessageReceived(int client, const char* msg, int length){
 
         if(url[0] == '?'){ // search query , has to be preceded by / because files cant be named as such 
             cpr::AsyncResponse resp_var = cpr::GetAsync(cpr::Url{"https://api.exa.ai/search"},
-                                cpr::Header{"Content-Type", "application/json"},
                                 cpr::Header{"x-api-key", (const char*)key},
                                 cpr::Payload{{"type","fast"},{"query","testing hello world"}}
                             );
