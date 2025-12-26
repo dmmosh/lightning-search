f8ad64772d00fa58a042402c8c5b0468a211eba2
wettestsock <evilsharkeater@gmail.com>
2025-12-26 16:29:25 -0500

    nothing to note

--- src/WebServer.cpp
+++ src/WebServer.cpp
@@ -328,7 +328,7 @@ void WebServer::onMessageReceived(int client, const char* msg, int length){
     std::cout << oss << '\n';
     
     oss = compressGzip(oss);
-    size = oss.size();
+    size = oss.size()+1;
 //    std::ostringstream oss; // output stream
     oss.insert(0,             std::format("HTTP/1.1 {} OK\r\n"
                             "Content-Encoding: gzip\r\n"
