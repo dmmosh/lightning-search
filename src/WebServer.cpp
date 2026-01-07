ecacc0b2d8c8b89ed3932fffe5cc9a1ad6f9a4ae
dmmosh <evilsharkeater@gmail.com>
2026-01-06 23:13:42 -0500

    nothing to note

--- src/WebServer.cpp
+++ src/WebServer.cpp
@@ -233,7 +233,7 @@ void WebServer::onMessageReceived(int client, const char* msg, int length){
         } else if(!strncmp(url, "ac?", 3)){ // autocomplete feature as defined by opensearch.xml file
             std::cout << parsed.substr(lastWord(parsed)) << '\n';
             //parsed = std::string("[\"")+ (parsed.c_str()+4) + "\", ["
-            parsed = std::string("[\"\""
+            parsed = std::string("[\"\",["
             "\"helbblo linkedin!\","
             "\"hebbdllo liankedin!\","
             "\"helcxclo lifdvfdnfkedin!\","
