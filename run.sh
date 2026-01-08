70debc10e0686fd80d873bba891252a5eb712256
dmmosh <evilsharkeater@gmail.com>
2026-01-07 23:27:09 -0500

    nothing to note

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ git add .
 git commit -am "nothing to note"
 git push > /dev/null
 
-g++ -std=c++20 -lz -lcpr src/main.cpp src/WebServer.cpp src/TcpListener.cpp -o ls
+g++ -std=c++20 -DGLOG_NO_EXPORT -lz -lcpr src/main.cpp src/WebServer.cpp src/TcpListener.cpp -o ls
