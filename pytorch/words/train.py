37d2143567614c134f306971fb12ba0b45a8751e
dmmosh <evilsharkeater@gmail.com>
2026-01-11 15:54:21 -0500

    nothing of note

--- pytorch/words/train.py
+++ pytorch/words/train.py
@@ -114,9 +114,8 @@ def is_number(s:str):
 for word in set([ word for title in list(ds_stack.take(25000)['title']) for word in title.split()]):
     
     curr = word
-    print(type(curr))
     
-    if(not curr.isascii() or curr.count('/')> 1 or curr.count('\\')>1): # if a file path (most have more than 1 directory)
+    if(not curr.isascii() or (curr.count('/')> 1 or curr.count('\\')>1)): # if a file path (most have more than 1 directory)
         continue
     
     # curr = curr.encode('ascii') # to lowercase and encodes in ascii
