65141e0479c79a5f617c34e123372c575b937b3f
dmmosh <evilsharkeater@gmail.com>
2026-01-11 16:53:43 -0500

    nothing of note

--- pytorch/words/train.py
+++ pytorch/words/train.py
@@ -126,7 +126,7 @@ for sentence in so: # for every sentence
         """
         if(curr.rfind('(') >0 and curr.count(')') == 0 and not curr[0].isdigit()): # if potentially a function , meaning ( is after 1st index, no closing brackets, and doesnt start with a digit
             j =i+1
-            while(j<num_words-1 and ')' not in words[j] and j-i-1<5): # until reaches last value, closing bracket, or more than 5 values 
+            while(j<num_words-1 and j-i-1<5): # until reaches last value, closing bracket, or more than 5 values 
                 if(')' in  words[j]): # if function, 
                     curr = ' '.join(words[i:j+1])
                     print(curr)
