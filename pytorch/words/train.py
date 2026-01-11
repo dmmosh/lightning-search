af9aeb364ba6e13214d2bd7e69354b14ae885a62
dmmosh <evilsharkeater@gmail.com>
2026-01-11 16:50:17 -0500

    nothing of note

--- pytorch/words/train.py
+++ pytorch/words/train.py
@@ -130,7 +130,7 @@ for sentence in so: # for every sentence
             while(j<num_words-1 and ')' not in words[j] and j-i-1<5): # until reaches last value, closing bracket, or more than 5 values 
                 j+=1
             if(')' in  words[j]): # if function, 
-                curr = ' '.join(words[i:j])+ ' ' + words[j].rstrip('\"\'`.!?,;:=')
+                curr = ' '.join(words[i:j+1])
                 print(curr)
                 i=j
                 
