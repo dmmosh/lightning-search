e8dcf68233a9e7e38d5dc45b67c563f00a131c1e
dmmosh <evilsharkeater@gmail.com>
2026-01-11 17:09:36 -0500

    nothing of note

--- pytorch/words/train.py
+++ pytorch/words/train.py
@@ -104,7 +104,7 @@ def is_num(s:str):
     
 # do some operations on the entire dictionary
 #so = list(set([ word for title in list(so_stack.take(25000)['title']) for word in title.split()])) 
-so = list(so_stack.take(100000)['title']) # stack overflow questions, each string is a question
+so = list(so_stack.take(25000)['title']) # stack overflow questions, each string is a question
 
 
 for sentence in so: # for every sentence
@@ -124,10 +124,16 @@ for sentence in so: # for every sentence
         could be the beginning of a function call, ? 
         how to handle? iterate 5 indeces 
         """
+        open_ctr = 1 # the amount of open brackets
         if(curr.lstrip('(\"\'`,!?;:=').rfind('(') >0 and curr.count(')') == 0 and not curr[0].isdigit()): # if potentially a function , meaning ( is after 1st index, no closing brackets, and doesnt start with a digit
             j =i+1
             while(j<num_words-1 and j-i-1<5): # until reaches last value, closing bracket, or more than 5 values 
+                if('(' in words[j]):
+                    open_ctr+=1
                 if(')' in  words[j]): # if function, 
+                    open_ctr-=1
+                
+                if(open_ctr == 0):
                     curr = ' '.join(words[i:j]) + ' ' + words[j][:max(words[j].rfind(')'),words[j].rfind(']'))+1]
                     print('before', curr)
                     i=j
