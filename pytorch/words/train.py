f49538449c387bbdbde95f94796809b927581e59
dmmosh <evilsharkeater@gmail.com>
2026-01-11 16:19:06 -0500

    nothing of note

--- pytorch/words/train.py
+++ pytorch/words/train.py
@@ -108,6 +108,7 @@ so = list(so_stack.take(25000)['title']) # stack overflow questions, each string
 
 
 for sentence in so: # for every sentence
+    print(sentence)
     words = str(sentence).split() # split by word
     num_words = len(words) # get length 
     
@@ -115,6 +116,7 @@ for sentence in so: # for every sentence
     i = 0
     while(i<num_words):
         curr = words[i]
+        curr= curr.lower()
 
         if(not curr.isascii() or curr.count('/')> 1 or curr.count('\\')>0): # if a file path (most have more than 1 directory) or on WINDOWS (ew)
             continue
